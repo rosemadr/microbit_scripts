04eb7eb56d58a6d3a9a4a1bb8d11c9c55c3c1cb3
Madi Simcock-Brown <m.simcock-brown@outlook.com>
2025-09-21 19:40:44 +0100

    added basic logic for 6 sided die roll

--- die.py
+++ die.py
@@ -1,6 +1,23 @@
 # Write your code here :-)
 from microbit import *
+import randint
 
 while True:
-    reading = accelerometer.get_x()
+    display.scroll('roll your die')
+    if accelerometer.was_gesture('shake'):
+        # drum roll?
+        roll_outcome = roll_die(6)
+        music_outcome = ''
+        if roll_outcome == 1:
+            music_outcome = music.WAWAWAWAA
+        elif roll_outcome == 6:
+            music_outcome = music.POWER_UP
+        else: music_outcome = music.BA_DING
+        display.show(roll_outcome + '!')
+        music.play(music_outcome)
+
+
+def roll_die(die_size):
+    return random.randint(1, die_size)
+
 
